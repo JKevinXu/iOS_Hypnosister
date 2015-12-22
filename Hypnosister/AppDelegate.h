068f154c65aa8ca058e2a5057d04a88f04d01a96
XuJian <xujian@xujians-macbook-pro.local>
2015-12-22 13:34:38 -0800

    Initial Commit

---
+++ Hypnosister/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Hypnosister
+//
+//  Created by XuJian on 12/22/15.
+//  Copyright © 2015 Jian (Kevin) Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Hypnosister/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Hypnosister
+//
+//  Created by XuJian on 12/22/15.
+//  Copyright © 2015 Jian (Kevin) Xu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
